5137dfb710938ad82b5877654c7cb44bd5672384
Prof-MohamedAtef <proofesser@gmail.com>
2018-11-23 00:52:51 -0800

    Dev App Server is now running|INFO:oejs\.Server:main:Started

---
+++ Jokingbackend/src/main/java/gradle/nanodegree/ed/mo/prof/builditbiggerfourth/MyBean.java
@@ -0,0 +1,15 @@
+package gradle.nanodegree.ed.mo.prof.builditbiggerfourth;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
