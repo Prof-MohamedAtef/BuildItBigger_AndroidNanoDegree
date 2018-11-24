fbcdcea68b3f52c99cf832794a0d1aa3fa6707c8
Prof-MohamedAtef <proofesser@gmail.com>
2018-11-24 07:46:03 -0800

    Editing MainActivityFragment

--- app/src/free/java/gradle/nanodegree/ed/mo/prof/builditbiggerfourth/MainActivityFragment.java
+++ app/src/free/java/gradle/nanodegree/ed/mo/prof/builditbiggerfourth/MainActivityFragment.java
@@ -12,33 +12,63 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 import gradle.nanodegree.ed.mo.prof.jokingandroidlib.JokeActivity;
 
 /**
  * Created by Prof-Mohamed Atef on 11/23/2018.
  */
 
-public class PaidActivityFragment extends Fragment implements OnTaskCompleted {
+public class MainActivityFragment extends Fragment implements OnTaskCompleted{
+
     private static String PhysicalIPAddress="http://192.168.1.2:8080/_ah/api/";
     private static String EmulatorIPAddress="http://10.0.2.2:8080/_ah/api/";
 
     Button BTNFetchJoke;
     ProgressDialog mProgressDialog;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setTheme(R.style.PaidTheme);
-    }
+    AdView adView;
+    AdRequest adRequest;
+    InterstitialAd interstitialAd;
+    private boolean adOnScreen=false;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_paid, container, false);
+        View view=inflater.inflate(R.layout.fragment_main, container, false);
         BTNFetchJoke=(Button)view.findViewById(R.id.btn_Fetch);
+        if (BuildConfig.FREE_VERSION){
+            adView=(AdView) view.findViewById(R.id.adView);
+            AdRequest adRequest=new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            adView.loadAd(adRequest);
+
+            interstitialAd=new InterstitialAd(getActivity());
+            interstitialAd.setAdUnitId(getString(R.string.interstial_unit_test_id));
+
+            interstitialAd.setAdListener(new AdListener(){
+                @Override
+                public void onAdClosed() {
+                    super.onAdClosed();
+                    adOnScreen = false;
+                    startNewInterstitialAd();
+                }
+            });
+
+        }
         BTNFetchJoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (interstitialAd.isLoaded()){
+                    adOnScreen=true;
+                    interstitialAd.show();
+                }else {
+                    adOnScreen=false;
+                }
                 getJokes();
                 mProgressDialog = ProgressDialog.show(getActivity(), getString(R.string.loading_msg), getString(R.string.pls_wait), true);
             }
@@ -46,8 +76,15 @@ public class PaidActivityFragment extends Fragment implements OnTaskCompleted {
         return view;
     }
 
+    private void startNewInterstitialAd() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        interstitialAd.loadAd(adRequest);
+    }
+
     private void getJokes() {
-        GCEndPointAsyncTask gcEndPointAsyncTask= new GCEndPointAsyncTask(PaidActivityFragment.this);
+        GCEndPointAsyncTask gcEndPointAsyncTask= new GCEndPointAsyncTask(MainActivityFragment.this);
         gcEndPointAsyncTask.execute(EmulatorIPAddress);
     }
 
@@ -55,11 +92,12 @@ public class PaidActivityFragment extends Fragment implements OnTaskCompleted {
     private static String Failed10022="failed to connect to /10.0.2.2 (port 8080) after 20000ms";
     private static String ECONNREFUSED="failed to connect to /"+ PhysicalIPAddress +"(port 8080) after 20000ms: isConnected failed: ECONNREFUSED (Connection refused)";
 
-
     @Override
     public void onTaskCompleted(String result) {
+        adOnScreen=false;
         mProgressDialog.dismiss();
         if (result.length()>0){
+
             if (result.equals(ConnTimedOut)||result.equals(ECONNREFUSED)||result.equals(Failed10022)){
                 Toast.makeText(getActivity(), ConnTimedOut, Toast.LENGTH_SHORT).show();
             }else {
@@ -72,6 +110,8 @@ public class PaidActivityFragment extends Fragment implements OnTaskCompleted {
 
     @Override
     public void onTaskError() {
-        Toast.makeText(getActivity(), getResources().getString(R.string.Whatisyourjoke), Toast.LENGTH_SHORT).show();
+        adOnScreen=false;
+        mProgressDialog.dismiss();
+        Toast.makeText(getActivity(), getString(R.string.nnnn), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/gradle/nanodegree/ed/mo/prof/builditbiggerfourth/MainActivity.java
+++ app/src/main/java/gradle/nanodegree/ed/mo/prof/builditbiggerfourth/MainActivity.java
@@ -13,24 +13,10 @@ import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.Fragment_container)
-    android.widget.FrameLayout FrameLayout;
-    private Unbinder unbinder;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        unbinder= ButterKnife.bind(this);
-        MainActivityFragment mainActivityFragment=new MainActivityFragment();
-
-        if (BuildConfig.FREE_VERSION){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(FrameLayout.getId(), mainActivityFragment, "Tag").commit();
-        }else if (!BuildConfig.FREE_VERSION){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(FrameLayout.getId(), new PaidActivityFragment(), "Tag").commit();
-        }
     }
 
     @Override
